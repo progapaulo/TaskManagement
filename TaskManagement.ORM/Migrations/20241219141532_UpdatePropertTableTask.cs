4a10b9c72ecfc7f250b395002e51890f0416e34b
Paulo Silva <progapaulo@gmail.com>
2024-12-19 13:00:30 -0300

    :raw alterações em tabelas

---
+++ TaskManagement.ORM/Migrations/20241219141532_UpdatePropertTableTask.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagement.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePropertTableTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Tasks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "String",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Priority",
+                table: "Tasks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "string",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Tasks",
+                type: "String",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Priority",
+                table: "Tasks",
+                type: "string",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
